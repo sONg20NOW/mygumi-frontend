6b0fecb7eee00ddddc450ce09e214372e23ab5f0
송강규 <sgk1004s@naver.com>
2026-07-14 17:34:26 +0900

    fix: @ 기호는 src 폴더로 이동

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path' // 1. path 모듈 import
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // 2. '@' 기호를 프로젝트 루트의 'src' 디렉토리로 매핑
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
